f6fa6c1318467033e832b29cd8eb55670ecc6306
prmnaa30 <pramanakadek335@gmail.com>
2026-04-09 16:37:35 +0800

    Revert "feat(be): add updateBook endpoint with old image cleanup"
    This reverts commit 34c1c7e80c2f4b193cf28ba2b88c04099f2c9e03.

--- backend/src/controllers/bookController.ts
+++ backend/src/controllers/bookController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/client.js";
-import path from "path";
-import fs from 'fs'
 
 export const getAllBooks = async (req: Request, res: Response) => {
   try {
@@ -34,38 +32,5 @@ export const createBook = async (req: Request, res: Response): Promise<any> => {
     return res.status(201).json({ message: "Buku berhasil ditambahkan", data: newBook })
   } catch (error) {
     return res.status(500).json({ message: "Gagal menambah buku" })
-  }
-}
-
-export const updateBook = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params
-    const price = req.body.price ? parseInt(req.body.price, 10) : 0
-    const { title, author, description, imageUrl } = req.body
-
-    const existingBook = await prisma.book.findUnique({
-      where: { id: Number(id) }
-    })
-
-    if (!existingBook) {
-      return res.status(401).json({ message: "Buku tidak ditemukan"})
-    }
-
-    if (imageUrl && existingBook.imageUrl) {
-      const oldImagePath = path.join(path.resolve("uploads"), existingBook.imageUrl)
-
-      if (fs.existsSync(oldImagePath)) {
-        fs.unlinkSync(oldImagePath)
-      }
-    }
-
-    const updatedBook = await prisma.book.update({
-      where: { id: Number(id) },
-      data: { title, author, price, description, imageUrl }
-    })
-
-    return res.status(201).json({ message: `Detail buku "${updatedBook.title}" berhasil diubah!`})
-  } catch (error) {
-    return res.status(500).json({ message: "Gagal mengubah detail buku" })
   }
 }
--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -1,12 +1,11 @@
 import express from 'express'
-import { createBook, getAllBooks, updateBook } from '../controllers/bookController.js' 
+import { createBook, getAllBooks } from '../controllers/bookController.js' 
 import { processImage, upload } from '../middlewares/uploadMiddleware.js'
 import { verifyToken } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/', getAllBooks)
-router.post('/', verifyToken, upload.single("book_image"), processImage, createBook)
-router.put('/:id', verifyToken, upload.single("book_image"), processImage, updateBook)
+router.post('/', verifyToken, upload.single("image"), processImage, createBook)
 
 export default router
